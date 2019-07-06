43d1dff7488793e4ba524d102ac3466387d8150e
Isale <bryanaim00@gmail.com>
2019-07-06 17:35:16 +0300

    Updated some features
    Created some helper functions, created database and tested the connection and updated the categories section using php to fetch from database

--- resources/config.php
+++ resources/config.php
@@ -13,7 +13,7 @@ defined("TEMPLATE_BACK") ? null: define("TEMPLATE_BACK", __DIR__ . DS . "templat
 defined("DB_HOST") ? null: define("DB_HOST","localhost");
 defined("DB_USER") ? null: define("DB_USER","root");
 defined("DB_PASS") ? null: define("DB_PASS","");
-defined("DB_NAME") ? null: define("DB_NAME","IsaleHomestore_db");
+defined("DB_NAME") ? null: define("DB_NAME","isaleHomestore_db");
 
 // create connection to the databases using mysqli
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
